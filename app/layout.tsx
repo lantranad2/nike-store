bc26784e8dae76afede75a142081a4d183245033
LanAd2 <tranthanhlan.ad2@gmail.com>
2023-01-08 19:41:38 +0700

    Initial commit.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,21 @@
-import './globals.css'
+import "../styles/globals.css";
+import { Poppins, Inter } from "@next/font/google";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["400"],
+  variable: "--font-poppins",
+});
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
       <head />
-      <body>{children}</body>
+      <body className={`${poppins.variable} font-sans`}>{children}</body>
     </html>
-  )
+  );
 }
