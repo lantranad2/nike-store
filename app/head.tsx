2061b363f4c9280af08e56eca4d72363a1251edb
LanAd2 <tranthanhlan.ad2@gmail.com>
2023-01-21 11:00:54 +0700

    Add a favicon.

--- app/head.tsx
+++ app/head.tsx
@@ -4,7 +4,7 @@ export default function Head() {
       <title>Nike Store</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="" />
-      <link rel="icon" href="/favicon.ico" />
+      <link rel="icon" type="image/png" href="/favicon.png" />
     </>
   );
 }
